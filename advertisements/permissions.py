fe425145541060570b680cd8c91ee67b6a8f06d6
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-03-15 16:30:56 +1100

    Update permissions.py

--- advertisements/permissions.py
+++ advertisements/permissions.py
@@ -1,10 +1,10 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrAdmin(BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-        if request.user.is_superuser:
+        if request.method in SAFE_METHODS or request.user.is_superuser:
             return True
 
         return obj.creator == request.user
